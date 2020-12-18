802fb5c9abd88e2d38f40bfbf7c710a0cb2a18f7
bashforger <bash.forger@gmail.com>
2020-12-18 15:29:53 +0500

    Added delete, edit & update methods

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -17,4 +17,16 @@ class ArticlesController < ApplicationController
     @article.save
     redirect_to article_path(@article)
   end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+    flash.notice = "Article '#{@article.title}' Updated!"
+    redirect_to article_path(@article)
+  end
+  
 end
